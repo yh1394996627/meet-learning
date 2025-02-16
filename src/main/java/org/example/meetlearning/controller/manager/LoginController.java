31e170777f52969865f0383b80340925de08b1c7
yu <1394996627@qq.com>
2025-02-16 15:28:54 +0800

    -- 代理商设计接口

---
+++ src/main/java/org/example/meetlearning/controller/manager/LoginController.java
@@ -0,0 +1,20 @@
+package org.example.meetlearning.controller.manager;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "登陆接口")
+@RestController
+@Slf4j
+public class LoginController {
+
+
+
+
+
+
+
+
+}
