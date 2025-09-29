1aeffb9aacc35f49d4d0657fd7821f03de2111a7
yuhang <1394996627@qq.com>
2025-09-29 23:53:05 +0800

    -- 解决权限问题

--- src/main/java/org/example/meetlearning/controller/BaseHandler.java
+++ src/main/java/org/example/meetlearning/controller/BaseHandler.java
@@ -4,6 +4,7 @@ package org.example.meetlearning.controller;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apache.commons.lang3.StringUtils;
 import org.example.meetlearning.common.BusinessException;
+import org.springframework.util.Assert;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -30,6 +31,7 @@ public interface BaseHandler {
     @JsonIgnore
     default String getTalkToken() {
         String talkToken = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest().getHeader("talkToken");
+        Assert.isTrue(StringUtils.isNotEmpty(talkToken), "talk token is empty");
         if (StringUtils.isEmpty(talkToken)) {
             throw new BusinessException(401, "talk token expired");
         }
