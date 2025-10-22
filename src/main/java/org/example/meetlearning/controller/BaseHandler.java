a612f9c246b49401edf3b57b1866f9ecdd655d75
yu.hang <yu.hang@lingyiitech.com>
2025-10-22 10:11:42 +0800

    --

--- src/main/java/org/example/meetlearning/controller/BaseHandler.java
+++ src/main/java/org/example/meetlearning/controller/BaseHandler.java
@@ -2,20 +2,25 @@ package org.example.meetlearning.controller;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.example.meetlearning.common.BusinessException;
 import org.example.meetlearning.common.SpringContextHolder;
 import org.example.meetlearning.dao.entity.User;
 import org.example.meetlearning.service.impl.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Objects;
 
+@
 
 public interface BaseHandler {
 
+    Logger log = LoggerFactory.getLogger(BaseHandler.class);
 
     @JsonIgnore
     default UserService getUserService() {
@@ -45,9 +50,11 @@ public interface BaseHandler {
             throw new BusinessException(401, "talk token expired");
         }
         User user = getUserService().selectByRecordId(getUserCode());
+        log.info("base---->user: {}", user);
         if (user != null) {
             Long pwVersion = user.getPwVersion();
             pwVersion = pwVersion == null ? 1 : pwVersion;
+            log.info("pwVersion: {},talkToken:{}", pwVersion, talkToken);
             if (!StringUtils.equals(talkToken, pwVersion.toString())) {
                 throw new BusinessException(401, "talk token expired");
             }
