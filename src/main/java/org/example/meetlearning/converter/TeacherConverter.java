b37f60a340c613fe42e933dfde847e7d7c1e8781
yu.hang <16220026+yuh95271@user.noreply.gitee.com>
2026-04-06 09:40:35 +0800

    -- 变更bug

--- src/main/java/org/example/meetlearning/converter/TeacherConverter.java
+++ src/main/java/org/example/meetlearning/converter/TeacherConverter.java
@@ -1,6 +1,7 @@
 package org.example.meetlearning.converter;
 
 import cn.hutool.core.util.BooleanUtil;
+import org.codehaus.plexus.util.StringUtils;
 import org.example.meetlearning.dao.entity.Teacher;
 import org.example.meetlearning.dao.entity.TeacherEvaluationRecord;
 import org.example.meetlearning.dao.entity.TeacherFeature;
@@ -86,6 +87,7 @@ public interface TeacherConverter {
         teacher.setGender(reqVo.getGender());
         teacher.setMeetLink(reqVo.getMeetLink());
         teacher.setMeetPassWord(reqVo.getMeetPassWord());
+        teacher.setZoomActivationStatus(StringUtils.isNotEmpty(reqVo.getMeetLink()));
         return teacher;
     }
 
